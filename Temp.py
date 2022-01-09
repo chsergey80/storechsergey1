d3f89fbb81410676bf241e3243767e31b4bbfa19
Sergey Chasovnikov <chsergey@yandex.ru>
2022-01-09 16:09:48 +0300

    ver2

--- Temp.py
+++ Temp.py
@@ -3,6 +3,6 @@ print('Вводите цены; для остановки введите -1.')
 price = float(input())
 while price > 0:
     total = total + price  # можно заменить на аналогичную запись
-    # total += price
+    # total += price  234242342334234234234234
     price = float(input())
 print('Общая стоимость равна', total)
